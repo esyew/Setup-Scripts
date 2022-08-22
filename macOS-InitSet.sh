ca9ed0dd3a478afdc295ee33a9939e67224567fe
esyew <erickrgrant@gmail.com>
2022-08-21 17:01:52 -0700

    Create macOS-InitSet.sh

---
+++ macOS-InitSet.sh
@@ -0,0 +1,6 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask github
+brew install handbrake
+brew install google-chrome
+brew install --cask spotify
