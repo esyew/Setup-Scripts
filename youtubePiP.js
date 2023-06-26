3a35473d9f109eb0fc9dfeaead5556c1b7a07425
Erick <erickrgrant@gmail.com>
2023-06-26 00:45:20 -0700

    Create youtubePiP.js
    Plugin for YouTube

---
+++ youtubePiP.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Youtube PiP
+// @description Youtube Picture in Picture Mod
+// @match       *://*/*
+// ==/UserScript==
+
+document.body.addEventListener("keydown", function(event) {
+    if (event.keyCode == 80) {
+        PiPer_1.webkitSetPresentationMode(PiPer_1.webkitPresentationMode === "picture-in-picture" ? "inline" : "picture-in-picture");
+    }
+});
+// Add this file to a extension that adds custom javascript to websites. Makes the 'P' key enable picture in picture on macOS.
