d12962fdd7e45f45659dcefc62c95d4bf64a7ef7
Erick <erickrgrant@gmail.com>
2023-06-28 13:58:28 -0700

    Update youtubePiP.js

--- youtubePiP.js
+++ youtubePiP.js
@@ -1,3 +1,5 @@
+// Add this file to a extension that adds custom javascript to websites. Makes the 'P' key enable picture in picture on macOS Safari.
+
 // ==UserScript==
 // @name        Youtube PiP
 // @description Youtube Picture in Picture Mod
@@ -9,4 +11,4 @@ document.body.addEventListener("keydown", function(event) {
         PiPer_1.webkitSetPresentationMode(PiPer_1.webkitPresentationMode === "picture-in-picture" ? "inline" : "picture-in-picture");
     }
 });
-// Add this file to a extension that adds custom javascript to websites. Makes the 'P' key enable picture in picture on macOS.
+
